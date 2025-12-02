203f5331b114bb545f94c7b63274d88af461f631
MUKTHARS <mohamedmukthar50@gmail.com>
2025-12-02 11:21:08 +0530

    pppp

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -179,66 +179,60 @@ return (
         </div>
         
         <div className="services-showcase-grid">
-          <div className="service-showcase-card">
-            <div className="service-icon-wrapper">
-              <div className="service-icon">🛣🏞🗺</div>
-              <div className="icon-backdrop"></div>
-            </div>
-            <h3>Road Construction</h3>
-            <p>Complete road development solutions including BT works, drainage systems, and quality-approved execution</p>
-            <ul className="service-features">
-              <li>Highway Development</li>
-              <li>Municipal Roads</li>
-              <li>Village Panchayat Roads</li>
-              <li>Quality Assured</li>
-            </ul>
-          </div>
+  <div className="service-showcase-card">
+    <div className="service-icon-wrapper">
+      <div className="service-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M4.5 9a.5.5 0 0 0-.5.5v1.5a.5.5 0 0 0 1 0V10h1v3.5a.5.5 0 0 0 1 0v-6a.5.5 0 0 0-.5-.5h-2ZM7 10h1v3.5a.5.5 0 0 0 1 0v-6a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 1 0V10Z"/>
+          <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12ZM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2Z"/>
+        </svg>
+      </div>
+      <div className="icon-backdrop"></div>
+    </div>
+    <h3>Road Construction</h3>
+    <p>Complete road development solutions including BT works, drainage systems, and quality-approved execution</p>
+  </div>
 
-          <div className="service-showcase-card">
-            <div className="service-icon-wrapper">
-              <div className="service-icon">🏙</div>
-              <div className="icon-backdrop"></div>
-            </div>
-            <h3>Building Construction</h3>
-            <p>Commercial and residential construction with modern techniques and premium materials</p>
-            <ul className="service-features">
-              <li>Residential Complexes</li>
-              <li>Commercial Buildings</li>
-              <li>Government Projects</li>
-              <li>Quality Monitoring</li>
-            </ul>
-          </div>
+  <div className="service-showcase-card">
+    <div className="service-icon-wrapper">
+      <div className="service-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M12.5 3a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1h5zm0 3a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1h5zm.5 3.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 .5-.5zm-.5 2.5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1h5z"/>
+          <path d="M16 2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2zM4 1v14H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h2zm1 0h9a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5V1z"/>
+        </svg>
+      </div>
+      <div className="icon-backdrop"></div>
+    </div>
+    <h3>Building Construction</h3>
+    <p>Commercial and residential construction with modern techniques and premium materials</p>
+  </div>
 
-          <div className="service-showcase-card">
-            <div className="service-icon-wrapper">
-              <div className="service-icon">⊞</div>
-              <div className="icon-backdrop"></div>
-            </div>
-            <h3>Layout Development</h3>
-            <p>Professional layout planning and development with proper infrastructure and utilities</p>
-            <ul className="service-features">
-              <li>Site Planning</li>
-              <li>Infrastructure Setup</li>
-              <li>Utility Management</li>
-              <li>Approved Layouts</li>
-            </ul>
-          </div>
+  <div className="service-showcase-card">
+    <div className="service-icon-wrapper">
+      <div className="service-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+        </svg>
+      </div>
+      <div className="icon-backdrop"></div>
+    </div>
+    <h3>Layout Development</h3>
+    <p>Professional layout planning and development with proper infrastructure and utilities</p>
+  </div>
 
-          <div className="service-showcase-card">
-            <div className="service-icon-wrapper">
-              <div className="service-icon">⛟</div>
-              <div className="icon-backdrop"></div>
-            </div>
-            <h3>Vehicle & Equipment</h3>
-            <p>Complete fleet of construction vehicles and equipment available for rental</p>
-            <ul className="service-features">
-              <li>Tipper Trucks</li>
-              <li>JCB & Excavators</li>
-              <li>Concrete Mixers</li>
-              <li>Well Maintained</li>
-            </ul>
-          </div>
-        </div>
+  <div className="service-showcase-card">
+    <div className="service-icon-wrapper">
+      <div className="service-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+        </svg>
+      </div>
+      <div className="icon-backdrop"></div>
+    </div>
+    <h3>Vehicle & Equipment</h3>
+    <p>Complete fleet of construction vehicles and equipment available for rental</p>
+  </div>
+</div>
       </section>
 
 {/* Premium Materials Section */}
