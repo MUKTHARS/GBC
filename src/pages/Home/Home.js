55369de3fb7961695946e1dfddda02cde713eb9b
MUKTHARS <mohamedmukthar50@gmail.com>
2025-12-02 12:43:57 +0530

    pppp

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -130,13 +130,13 @@ return (
                   <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
               </a>
-              {/* <a href="/contact" className="btn btn-secondary">
+              <a href="/contact" className="btn btn-secondary">
                 <span>Get Free Quote</span>
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
                   <path d="M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2C16.75 2 21 6.25 21 11.5Z" stroke="currentColor" strokeWidth="2"/>
                   <path d="M16 11.5H11.5M11.5 11.5L11.5 7M11.5 11.5L7 11.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
                 </svg>
-              </a> */}
+              </a>
             </div>
           </div>
           <div className="hero-visual">
